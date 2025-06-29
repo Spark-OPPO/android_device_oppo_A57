58d29a219cd5f07990dea21af19bcc1d9eb3c047
Ricky Cheung <rcheung844@gmail.com>
2025-06-30 02:45:54 +0800

    A57: Removed fixup for libmmcamera_dbg.so
    This blob is located in thortanium-common repo.
    
    Change-Id: I373441da260e7ef5967cb8334b12767b6fc8dda5

--- extract-files.sh
+++ extract-files.sh
@@ -28,9 +28,6 @@ function blob_fixup() {
         vendor/bin/mm-qcamera-daemon|vendor/lib/libmmcamera2_cpp_module.so|vendor/lib/libmmcamera2_dcrf.so|vendor/lib/libmmcamera2_iface_modules.so|vendor/lib/libmmcamera2_imglib_modules.so|vendor/lib/libmmcamera2_mct.so|vendor/lib/libmmcamera2_pproc_modules.so|vendor/lib/libmmcamera2_q3a_core.so|vendor/lib/libmmcamera2_stats_algorithm.so|vendor/lib/libmmcamera_imglib.so|vendor/lib/libmmcamera_pdaf.so|vendor/lib/libmmcamera_pdafcamif.so|vendor/lib/libmmcamera_tintless_algo.so|vendor/lib/libmmcamera_tintless_bg_pca_algo.so|vendor/lib/libmmcamera_tuning.so|vendor/lib/libremosaic_daemon.so|vendor/lib64/libremosaic_daemon.so)
             sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${2}"
             ;;
-        vendor/lib/libmmcamera_dbg.so)
-            sed -i "s|/data/vendor/camera/|///data/vendor/qcam/|g" "${2}"
-            ;;
         vendor/lib/libmmcamera2_sensor_modules.so)
             sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "${2}"
             sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${2}"
