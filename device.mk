86d311b96f679a9a4a902a1ad8f8a789269a7279
Ricky Cheung <rcheung844@gmail.com>
2025-07-06 20:12:13 +0800

    A57: audio: Import audio platform info and mixer paths
    From thortanium-common repo, which stored the version of config
    for this device.
    
    Change-Id: I1308a45afed3d9cae19d71726de7fd9edffcfabe

--- device.mk
+++ device.mk
@@ -25,6 +25,11 @@ TARGET_SCREEN_WIDTH := 720
 # Device uses high-density artwork where available
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(DEVICE_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+
 # Input
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/oppo_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/oppo_touchscreen.kl
