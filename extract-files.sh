f11510e0ef5e5943c0f173a54be67c886bd4c1df
Ricky Cheung <rcheung844@gmail.com>
2025-07-20 01:47:42 +0800

    Revert "A57: Remove legacy RIL files"
    This reverts commit 0d87f6976dc492f6764fcc97bb7da83ca2594613.
    
    Change-Id: I09b41dbf564522b59820fa5023a3c11223e43289

--- extract-files.sh
+++ extract-files.sh
@@ -21,6 +21,10 @@ function blob_fixup() {
         vendor/lib64/libalipay_factory.so|vendor/lib64/lib_fpc_tac_shared.so)
             sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
             ;;
+        vendor/lib64/libril-qc-qmi-1.so)
+            sed -i "s|libqmiservices.so|libQmiservices.so|g" "${2}"
+            "${PATCHELF_0_17_2}" --replace-needed "libmedia.so" "libshims_legacy_ril.so" "${2}"
+            ;;
         vendor/bin/mm-qcamera-daemon|vendor/lib/libmmcamera2_cpp_module.so|vendor/lib/libmmcamera2_dcrf.so|vendor/lib/libmmcamera2_iface_modules.so|vendor/lib/libmmcamera2_imglib_modules.so|vendor/lib/libmmcamera2_mct.so|vendor/lib/libmmcamera2_pproc_modules.so|vendor/lib/libmmcamera2_q3a_core.so|vendor/lib/libmmcamera2_stats_algorithm.so|vendor/lib/libmmcamera_imglib.so|vendor/lib/libmmcamera_pdaf.so|vendor/lib/libmmcamera_pdafcamif.so|vendor/lib/libmmcamera_tintless_algo.so|vendor/lib/libmmcamera_tintless_bg_pca_algo.so|vendor/lib/libmmcamera_tuning.so|vendor/lib/libremosaic_daemon.so|vendor/lib64/libremosaic_daemon.so)
             sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${2}"
             ;;
