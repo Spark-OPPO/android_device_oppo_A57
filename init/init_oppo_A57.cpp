f591a7636cdb28175ce7c255b4040b378126e17d
Ricky Cheung <rcheung844@gmail.com>
2025-07-20 17:07:45 +0800

    A57: init: Set IMEI SV directly
    This change is required after common repo commit d2ea085fe
    ("thortanium-common: radio: Inject read IMEI from legacy RILD").
    
    Change-Id: Idedba7fb8c9b0c60faf724d831b4f70210149e7d

--- init/init_oppo_A57.cpp
+++ init/init_oppo_A57.cpp
@@ -23,7 +23,7 @@ static const variant_info_t a57_info = {
     .model = "OPPO A57",
     .build_fingerprint = "OPPO/A57/A57:6.0.1/MMB29M/1527754036:user/release-keys",
     .build_description = "msm8937_64-user 6.0.1 MMB29M eng.root.20191205.095236 dev-keys",
-    .build_ota = "A57_11.A.32_0320_201912050917",
+    .imei_sv = 32,
 };
 
 static const variant_info_t a57t_info = {
@@ -33,7 +33,7 @@ static const variant_info_t a57t_info = {
     .model = "OPPO A57t",
     .build_fingerprint = "OPPO/A57t/A57:6.0.1/MMB29M/1527754036:user/release-keys",
     .build_description = "msm8937_64-user 6.0.1 MMB29M eng.root.20191205.101424 dev-keys",
-    .build_ota = "A57t_11.A.22_0220_201912050917",
+    .imei_sv = 22,
 };
 
 static const variant_info_t cph1701_info = {
@@ -43,7 +43,7 @@ static const variant_info_t cph1701_info = {
     .model = "CPH1701",
     .build_fingerprint = "Android/msm8937_64/msm8937_64:6.0.1/MMB29M/root10091402:user/release-keys",
     .build_description = "msm8937_64-user 6.0.1 MMB29M eng.root.20181009.140111 release-keys",
-    .build_ota = "CPH1701EX_11.A.36_INT_036_201810091339",
+    .imei_sv = 36,
 };
 
 static const variant_info_t cph1701fw_info = {
@@ -53,7 +53,7 @@ static const variant_info_t cph1701fw_info = {
     .model = "CPH1701fw",
     .build_fingerprint = "Android/msm8937_64/msm8937_64:6.0.1/MMB29M/root10091402:user/release-keys",
     .build_description = "msm8937_64-user 6.0.1 MMB29M eng.root.20181009.140111 release-keys",
-    .build_ota = "CPH1701EX_11.A.36_INT_036_201810091339",
+    .imei_sv = 36,
 };
 
 static void determine_device() {
